ec047b60db0277b4ce7ec8f365f6f87493a13417
af1053 <aeflores@bancatlan.hn>
2019-12-18 16:08:16 -0600

    form

--- mern-stack/src/App.js
+++ mern-stack/src/App.js
@@ -1,23 +1,60 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Menu, Layout } from 'antd';
+import 'antd/dist/antd.css';
+//components
+import CreateStudent from './components/create-student.component';
+import EditStudent from './components/edit-student.component';
+import StudentList from './components/student-list.component';
+
+const { Header, Content , Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className='App' style={{ height: '100%' }}>
+        <Layout style={{ height: '100%' }}>
+          <Header className='App-header'>
+            <Menu theme="dark" mode="horizontal" style={{ lineHeight: '64px' }}>
+              <Menu.Item key='1'>
+                <Link to="./create-student">
+                  React MERN Stack App
+                </Link>
+              </Menu.Item>
+              <Menu.Item key='2'>
+                <Link to="./create-student">
+                  Create Student
+                </Link>
+              </Menu.Item>
+              <Menu.Item key='3'>
+                <Link to="./edit-student">
+                  Edit Student
+                </Link>
+              </Menu.Item>
+              <Menu.Item key='4'>
+                <Link to="./student-list">
+                  Student List
+                </Link>
+              </Menu.Item>
+            </Menu>
+          </Header>
+          <Content style={{ padding: '0 50px' }}>
+            <div style={{ background: '#fff', padding: 24, height: '100%' }}>
+              <Router>
+                <Route exact path='/' component={CreateStudent} />
+                <Route path='/create-student' component={CreateStudent} />
+                <Route path='/edit-student' component={EditStudent} />
+                <Route path='/student-list' component={StudentList} />
+              </Router>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Footer MERN Stack
+          </Footer>
+        </Layout>
+      </div>
+    
   );
 }
 
--- mern-stack/src/components/create-student.component.js
+++ mern-stack/src/components/create-student.component.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Form, Button, Input } from 'antd'
 
 export default function createStudent() {
     return (
         <div>
-            <p>React Create Student Component!</p>
+            <Form style={{ padding: '0 25%' }}>
+                <Form.Item label='Name'>
+                    <Input type="text"/>
+                </Form.Item>
+
+                <Form.Item label='Email'>
+                    <Input type="email"/>
+                </Form.Item>
+
+                <Form.Item label='Roll No'>
+                    <Input type="text"/>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+                        Create Student
+                    </Button>
+                </Form.Item>
+            </Form>
         </div>
     )
 }
